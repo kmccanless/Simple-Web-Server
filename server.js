c148e899daa94765021cd025315033b0e6d5612b
Keith McCanless <keith@keiths-mbp.lan>
2020-02-19 10:55:44 -0500

    trying again with env vars

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-var app = require('http').createServer(handler)
+var http = require('http')
     , fs = require('fs')
     , port 
     , file ;
 
-port = Number(process.argv[2]) || Number(process.env.PORT);
-file = process.argv[3] || process.env.FILE;
-app.listen(port);
+port = Number(process.env.PORT) || 8080;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+  }).listen(port);
 console.log("Server listening on port %s", port);
-
-function handler (req, res) {
-    fs.readFile(file,
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading file');
-            }
-            res.writeHead(200);
-            res.end(data);
-        });
-}
-
