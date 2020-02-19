746903aecac09b8bb7edcc89b71686222e231905
Keith McCanless <keith@keiths-mbp.lan>
2020-02-19 11:38:25 -0500

    pushing revisions

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http')
 port = Number(process.env.PORT) || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
+    res.write('Hello World Revision 1!');
     res.end();
   }).listen(port);
 console.log("Server listening on port %s", port);
