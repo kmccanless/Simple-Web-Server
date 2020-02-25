86f95efa3f0d056465966cafac721c824ed96193
Keith McCanless <keith@keiths-mbp.lan>
2020-02-25 10:44:53 -0500

    moved to env var fro message

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http')
-    , fs = require('fs')
-    , port 
-    , file ;
+    , port
+    , greeting;
 
 port = Number(process.env.PORT) || 8080;
+process.env.MESSAGE=""?greeting="Hello":greeting=process.env.MESSAGE;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World Revision 1!');
+    res.write(greeting);
     res.end();
   }).listen(port);
 console.log("Server listening on port %s", port);
