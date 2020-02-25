28250b152a6dcdb4a921ee4cac5bac75635fd796
Keith McCanless <keith@keiths-mbp.lan>
2020-02-25 11:14:25 -0500

    bug and errors

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var http = require('http')
     , greeting;
 
 port = Number(process.env.PORT) || 8080;
-process.env.MESSAGE=""?greeting="Hello":greeting=process.env.MESSAGE;
-http.createServer(function (req, res) {
+greeting = process.env.MESSAGE || "howdy";
+
+var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write(greeting);
     res.end();
   }).listen(port);
 console.log("Server listening on port %s", port);
+
+server.on('error',function(e){
+  console.log(e);
+})
