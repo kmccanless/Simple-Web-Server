870e190d61f4008959f9e045e8d5fc8d56636f68
Keith McCanless <keith@keiths-mbp.lan>
2020-03-04 12:07:19 -0500

    testing changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http')
     , greeting;
 
 port = Number(process.env.PORT) || 8080;
-greeting = process.env.MESSAGE || "hello";
+greeting = process.env.MESSAGE || "hello you";
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
